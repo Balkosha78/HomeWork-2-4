0669f8ef41c8bb7694e1c537636d9821c52d551c
Алексей <balko-aleksey@yandex.ru>
2023-04-14 00:19:23 +0700

    1

---
+++ src/main/java/pro/sky/skyprospringHWcalculator/SkyproSpringHwCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringHWcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHwCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHwCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringHWcalculator/SkyproSpringHwCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringHWcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringHwCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
