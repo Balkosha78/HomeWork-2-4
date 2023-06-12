fab12d30307f79b046839993640f0c3c1e8eb63a
Алексей <balko-aleksey@yandex.ru>
2023-06-12 14:18:15 +0700

    homework 2-13 completed

---
+++ src/test/java/pro/sky/skyprospringHWcalculator/service/CalculatorServiceImplParameterizedTest.java
@@ -0,0 +1,67 @@
+package pro.sky.skyprospringHWcalculator.service;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class CalculatorServiceImplParameterizedTest {          //2-13
+    private final CalculatorService calculatorService = new CalculatorServiceImpl();
+    private static Stream<Arguments> argumentProviderForCalculator(){
+        return Stream.of(
+                Arguments.of(10, 10),
+                Arguments.of(-10, -10),
+                Arguments.of(0, 0),
+                Arguments.of(0, 5),
+                Arguments.of(-5, 0));
+    }
+    private static Stream<Arguments> argumentProviderForCalculatorDivide(){
+        return Stream.of(
+                Arguments.of(10, 10),
+                Arguments.of(-10, -10),
+                Arguments.of(0, 5));
+    }
+    @ParameterizedTest
+    @MethodSource("argumentProviderForCalculator")
+    public void shouldCorrectlyCalculateSum(int num1, int num2){
+        //given
+        int expectedSum = num1 + num2;
+        //when
+        Integer actualSum = Integer.valueOf(calculatorService.plus(num1, num2));
+        //then
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+    @ParameterizedTest
+    @MethodSource("argumentProviderForCalculator")
+    public void shouldCorrectlyCalculateMinus(int num1, int num2){
+        //given
+        int expectedMinus = num1 - num2;
+        //when
+        Integer actualMinus = Integer.valueOf(calculatorService.minus(num1, num2));
+        //then
+        Assertions.assertEquals(expectedMinus, actualMinus);
+    }
+    @ParameterizedTest
+    @MethodSource("argumentProviderForCalculator")
+    public void shouldCorrectlyCalculateMultiply(int num1, int num2){
+        //given
+        int expectedMultiply = num1 * num2;
+        //when
+        Integer actualMultiply = Integer.valueOf(calculatorService.multiply(num1, num2));
+        //then
+        Assertions.assertEquals(expectedMultiply, actualMultiply);
+    }
+    @ParameterizedTest
+    @MethodSource("argumentProviderForCalculatorDivide")
+    public void shouldCorrectlyCalculateDivide(int num1, int num2){
+        //given
+        int expectedDivide = num1 / num2;
+        //when
+        Integer actualDivide = Integer.valueOf(calculatorService.divide(num1, num2));
+        //then
+        Assertions.assertEquals(expectedDivide, actualDivide);
+    }
+}
---
+++ src/test/java/pro/sky/skyprospringHWcalculator/service/CalculatorServiceImplTest.java
@@ -0,0 +1,62 @@
+package pro.sky.skyprospringHWcalculator.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceImplTest {          //2-13
+    private final CalculatorService calculatorService = new CalculatorServiceImpl();
+    @Test
+    public void shouldCorrectlyCalculateSum(){
+        //given
+        int num1 = 5;
+        int num2 = 5;
+        int expectedSum = num1 + num2;
+        //when
+        Integer actualSum = Integer.valueOf(calculatorService.plus(num1, num2));
+        //then
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+    @Test
+    public void shouldCorrectlyCalculateMinus(){
+        //given
+        int num1 = 5;
+        int num2 = 5;
+        int expectedMinus = num1 - num2;
+        //when
+        Integer actualMinus = Integer.valueOf(calculatorService.minus(num1, num2));
+        //then
+        Assertions.assertEquals(expectedMinus, actualMinus);
+    }
+    @Test
+    public void shouldCorrectlyCalculateMultiply(){
+        //given
+        int num1 = 5;
+        int num2 = 5;
+        int expectedMultiply = num1 * num2;
+        //when
+        Integer actualMultiply = Integer.valueOf(calculatorService.multiply(num1, num2));
+        //then
+        Assertions.assertEquals(expectedMultiply, actualMultiply);
+    }
+    @Test
+    public void shouldCorrectlyCalculateDivide(){
+        //given
+        int num1 = 5;
+        int num2 = 5;
+        int expectedDivide = num1 / num2;
+        //when
+        Integer actualDivide = Integer.valueOf(calculatorService.divide(num1, num2));
+        //then
+        Assertions.assertEquals(expectedDivide, actualDivide);
+    }
+    @Test
+    public void shouldThrowExceptionWhenSecondNumberIsZero(){
+        //given
+        int num1 = 5;
+        int num2 = 0;
+        //when
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Integer.valueOf(calculatorService.divide(num1, num2)); });
+    }
+}
